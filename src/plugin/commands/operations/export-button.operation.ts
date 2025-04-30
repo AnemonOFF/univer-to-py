30159c6cee3d17ec7f8fb4d01590c4f4b6518a5f
Anemon <admin@anemonlabs.ru>
2025-04-30 19:16:48 +0500

    Add undefined cell caught

--- src/plugin/commands/operations/export-button.operation.ts
+++ src/plugin/commands/operations/export-button.operation.ts
@@ -25,41 +25,51 @@ const download = (text: string) => {
 };
 
 const exportPython = (worksheet: Worksheet) => {
-  const sheetSnapshot = worksheet.getSnapshot();
-
-  const func = sheetSnapshot.cellData[1][0].f?.trim();
-  console.log("Целевая функция", func);
-  if (!func) {
-    alert("Укажите целевую функцию в ячейке A1");
-    return;
-  }
-
-  const restrictions = [];
-  for (const [, row] of Object.entries(sheetSnapshot.cellData)) {
-    const cell = row[1];
-    if (cell && cell.f) restrictions.push(cell.f.trim());
-  }
-  console.log("Ограничения", restrictions);
-
-  const variables = extractVariables([func, ...restrictions]).map((v) =>
-    v.trim()
-  );
-  console.log("Переменные", variables);
-  const varData = variables.reduce<Record<string, string>>((res, variable) => {
-    const row = A1.getRow(variable) - 1;
-    const col = A1.getCol(variable) - 1;
-    if (
-      !Object.keys(sheetSnapshot.cellData).includes(row.toString()) ||
-      !Object.keys(sheetSnapshot.cellData[row]).includes(col.toString())
-    )
-      return res;
-    const val = sheetSnapshot.cellData[row][col].v?.toString().trim();
-    if (!val) return res;
-    return { [variable]: val, ...res };
-  }, {});
-  console.log("Переменные", varData);
-
   try {
+    const sheetSnapshot = worksheet.getSnapshot();
+
+    if (
+      !Object.keys(sheetSnapshot.cellData).includes("1") ||
+      !Object.keys(sheetSnapshot.cellData[1]).includes("0")
+    ) {
+      alert("Укажите целевую функцию в ячейке A1");
+      return;
+    }
+    const func = sheetSnapshot.cellData[1][0].f?.trim();
+    console.log("Целевая функция", func);
+    if (!func) {
+      alert("Укажите целевую функцию в ячейке A1");
+      return;
+    }
+
+    const restrictions = [];
+    for (const [, row] of Object.entries(sheetSnapshot.cellData)) {
+      const cell = row[1];
+      if (cell && cell.f) restrictions.push(cell.f.trim());
+    }
+    console.log("Ограничения", restrictions);
+
+    const variables = extractVariables([func, ...restrictions]).map((v) =>
+      v.trim()
+    );
+    console.log("Переменные", variables);
+    const varData = variables.reduce<Record<string, string>>(
+      (res, variable) => {
+        const row = A1.getRow(variable) - 1;
+        const col = A1.getCol(variable) - 1;
+        if (
+          !Object.keys(sheetSnapshot.cellData).includes(row.toString()) ||
+          !Object.keys(sheetSnapshot.cellData[row]).includes(col.toString())
+        )
+          return res;
+        const val = sheetSnapshot.cellData[row][col].v?.toString().trim();
+        if (!val) return res;
+        return { [variable]: val, ...res };
+      },
+      {}
+    );
+    console.log("Переменные", varData);
+
     const pythonCode = Convert(func, restrictions, varData);
     download(pythonCode);
   } catch (ex) {
