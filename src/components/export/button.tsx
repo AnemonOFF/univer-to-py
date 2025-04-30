c39e2188fe6d6885a02742d9cf1f58c536107140
Anemon <admin@anemonlabs.ru>
2025-04-30 15:35:46 +0500

    Apply convert func and file download

--- src/components/export/button.tsx
+++ src/components/export/button.tsx
@@ -1,22 +1,42 @@
 import { FUniver, Univer } from "@univerjs/presets";
 import React from "react";
 import { Button } from "../ui/button";
+import { Convert } from "@/converter";
 
 const ExportButton: React.FC<{ univer: Univer; univerApi: FUniver }> = ({
   univerApi,
 }) => {
+  const download = (text: string) => {
+    const element = document.createElement("a");
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(text)
+    );
+    element.setAttribute("download", "code.py");
+
+    element.style.display = "none";
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  };
+
   const onExport = () => {
     const fWorkbook = univerApi.getActiveWorkbook()!;
     const fWorksheet = fWorkbook.getActiveSheet();
     const sheetSnapshot = fWorksheet.getSheet().getSnapshot();
-    const func = sheetSnapshot.cellData[1][0].f;
+    const func = sheetSnapshot.cellData[1][0].f as string;
     console.log("Целевая функция", func);
     const restrictions = [];
     for (const [, row] of Object.entries(sheetSnapshot.cellData)) {
       const cell = row[1];
-      if (cell) restrictions.push(cell.f);
+      if (cell && cell.f) restrictions.push(cell.f);
     }
     console.log("Ограничения", restrictions);
+
+    const pythonCode = Convert(func, restrictions);
+    download(pythonCode);
   };
 
   return <Button onClick={onExport}>Экспорт</Button>;
